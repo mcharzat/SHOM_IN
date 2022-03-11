eb7a73afd219af9faea1b84affdf22943339ac98
Maxime_Charzat <23charzatcav@gmail.com>
2022-03-11 17:05:16 +0100

    feat(map): Add first instance of map with shom wms

---
+++ shom_in/src/components/MapShom.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div id="map" class="map"></div>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+
+export default {
+  name: 'MapShom',
+  mounted() {
+    // Leaflet
+    const map = L.map('map').setView([51.505, -0.09], 7);
+    const tile = L.tileLayer.wms(
+      'https://masterTSI:fx7Hvd7J2BZF%40C@shom.wms.geomod.fr/KARMOR_MAP_SERVICES/wms',
+      {
+        layers: 'base',
+        format: 'image/png',
+      }
+    );
+    tile.addTo(map);
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#map {
+  height: 700px;
+  width: 100%;
+}
+</style>
