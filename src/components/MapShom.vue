145ecff18f5c3f2db37a66674cfa6f832303ada3
ljeannest <lucie.jeannest@orange.fr>
2022-03-15 15:31:35 +0100

    ajout de la div mouseTracker

--- src/components/MapShom.vue
+++ src/components/MapShom.vue
@@ -1,5 +1,8 @@
 <template>
-    <div id="map" class="map"></div>
+    <div id="map" class="map" ></div>
+    <div id="mouseTracker">
+        <p>{{ coordinate }}</p>
+    </div>
 </template>
 
 <script>
@@ -8,6 +11,11 @@ import L from 'leaflet';
 
 export default {
   name: 'MapShom',
+  data() {
+    return {
+      coordinate: "Mouse is not over map"
+    }
+  },
   mounted() {
     // Leaflet
     const map = L.map('map', {
@@ -28,14 +36,30 @@ export default {
     );
     tile.addTo(map);
   },
+  methods: {
+    getCoord: function (e) {
+      this.coordinate = e.latlng.toString();
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div {
+#map {
   height: 100%;
   width: 100%;
   z-index: 0;
+}
+#mouseTracker{
+  width:400px;
+  height:35px;
+  position: fixed;
+  bottom: 0px;
+  right: 100px;
+}
+#mouseTracker p{
+  background: white;
+  opacity: 85%;
 }
 </style>
