22f4a70ea7bc9ef9b5955e8a0789b4065dfe077a
LePaulM <p.miancien@laposte.net>
2022-03-16 17:35:48 +0100

    sidepanel available for pdfmanager

--- src/components/PDFManager.vue
+++ src/components/PDFManager.vue
@@ -1,11 +1,41 @@
 <template>
-    <button id="pdfManager" class="pdfManager" onclick='console.log("pdfManager")'></button>
+    <button id="pdfManager" class="pdfManager"></button>
+    <div id='mySidepanel'></div>
 </template>
 
 <script>
 export default {
   name: 'PDFManager',
+  data() {
+    return {
+      infSidePanel: false,
+      docSidePanel:null,
+      pdfManager:null
+    }
+  },
+  mounted() {
+    this.pdfManager = document.getElementById('pdfManager');
+    this.pdfManager.addEventListener("click", this.actionSidePanel);
+    this.docSidePanel = document.getElementById("mySidepanel");
+  },
+  methods : {
+    actionSidePanel() {
+      if (this.infSidePanel == false) {
+        console.log('rangement du panel');
+        this.docSidePanel.style.width = "250px";
+        this.pdfManager.style.right = "250px";
+        this.infSidePanel = true;
+      } else {
+        console.log('sortie du panel');
+        this.docSidePanel.style.width = "0px";
+        this.pdfManager.style.right = "0px";
+        this.infSidePanel = false;
+      }
+    }
+
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -16,18 +46,27 @@ export default {
 }
 
 .pdfManager {
-    position: absolute;
-    top: 100px;
-    right: 0px;
-    z-index: 25;
-    
-    background: url(../assets/texte.png);
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-color:lightblue;
+  position: absolute;
+  top: 100px;
+  right: 0px;
+  z-index: 25;
+  
+  background: url(../assets/texte.png);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-color:lightblue;
 
-    border:solid;
-    border-color: #076E8C;
-    border-width: 2.5px;
+  border:solid;
+  border-color: #076E8C;
+  border-width: 2.5px;
+}
+
+#mySidepanel {
+  position: absolute;
+  background-color: beige;
+  height: 100%;
+  right: 0px;
+  top : 70px;
+  z-index: 1000;
 }
 </style>
