cc3600ec371ee34c42b87dac8e9eac9da6c589fa
Maxime_Charzat <23charzatcav@gmail.com>
2022-03-17 15:35:57 +0100

    refactor(pdfManager): Refactor to use vue

--- src/components/PDFManager.vue
+++ src/components/PDFManager.vue
@@ -1,8 +1,8 @@
 <template>
-    <button id="pdfManager" class="pdfManager">
+    <button :class="{pdfManager: true, pdfManagerOpen: moveSidePanel}" @click="actionSidePanel">
       <img src="../assets/texte.png" height ="30" width="30"/>
     </button>
-    <div id='mySidepanel'></div>
+    <div :class="{pdfSidepanelOpen: moveSidePanel, pdfSidepanel: true}"></div>
 </template>
 
 <script>
@@ -11,43 +11,31 @@ export default {
   data() {
     return {
       infSidePanel: false,
-      docSidePanel:null,
-      pdfManager:null
     }
   },
   mounted() {
-    this.pdfManager = document.getElementById('pdfManager');
-    this.pdfManager.addEventListener("click", this.actionSidePanel);
-    this.docSidePanel = document.getElementById("pdfSidepanel");
-    this.viewport_height = window.innerHeight;
   },
-  methods : {
-    actionSidePanel() {
-      if (this.infSidePanel == false) {
-        this.docSidePanel.style.width = "250px";
-        this.docSidePanel.style.height = "600px";
-        this.pdfManager.style.right = "250px";
-        this.infSidePanel = true;
-      } else {
-        this.docSidePanel.style.width = "0px";
-        this.pdfManager.style.right = "5px";
-        this.infSidePanel = false;
-      }
+  computed : {
+    moveSidePanel() {
+      return this.infSidePanel;
     }
-  }
+  },
+  methods: {
+    actionSidePanel() {
+      this.infSidePanel = !this.infSidePanel;
+    }
+  },
 }
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#pdfManager {
-  height: 50px;
-  width: 50px;
-}
 
 .pdfManager {
   position: absolute;
+  height: 50px;
+  width: 50px;
   top: 100px;
   right: 5px;
   z-index: 25;
@@ -60,11 +48,22 @@ export default {
   box-shadow: 0 0 5px rgba(0,0,0,0.19), 0 0 5px rgba(0,0,0,0.19)
 }
 
-#pdfSidepanel {
+.pdfManagerOpen {
+  right: calc(33% + 10px);
+}
+
+.pdfSidepanel {
   position: absolute;
   background-color: beige;
   right: 0px;
   top : 100px;
   z-index: 1000;
+}
+
+.pdfSidepanelOpen {
+  right: 5px;
+  width: 33%;
+  height: 100%;
+  max-height: calc(100% - 125px);
 }
 </style>
