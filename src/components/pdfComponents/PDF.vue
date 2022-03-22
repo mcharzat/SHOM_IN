ba25a0a8f3c70341caaf1ee1ffeacced22d80180
ljeannest <lucie.jeannest@orange.fr>
2022-03-22 10:33:34 +0100

    pdf-viewer-vue

--- src/components/pdfComponents/PDF.vue
+++ src/components/pdfComponents/PDF.vue
@@ -1,13 +1,29 @@
 <template>
-  <pdf src="../../assets/C22.pdf"></pdf>
+  <div>
+    <h1>File</h1>
+
+    <PDFViewer
+      :source="url"
+      @download="handleDownload"
+    />
+  </div>
 </template>
 
 <script>
-import pdf from 'vue-pdf'
+import PDFViewer from 'pdf-viewer-vue'
+
+// OR THE FOLLOWING IMPORT FOR VUE 2
+// import PDFViewer from 'pdf-viewer-vue/dist/vue2-pdf-viewer'
 
 export default {
   components: {
-    pdf
-  }
+    PDFViewer,
+  },
+
+  data() {
+    return {
+      url: '../../assets/C22.pdf',
+    }
+  },
 }
 </script>
