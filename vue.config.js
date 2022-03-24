39cbe2c0000f7af3029f325ba9012ee42777ee96
Maxime_Charzat <23charzatcav@gmail.com>
2022-03-24 13:59:15 +0100

    config(loader): Add loader for ttl file

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
-
-    
+    module: {
+      rules: [
+        {
+          test: /\.ttl$/i,
+          loader: 'file-loader',
+        }
+      ]
+    }
   }
 })
