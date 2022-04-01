4c538ea77db72ae53ebe82877442ab147939fc89
ljeannest <lucie.jeannest@orange.fr>
2022-04-01 15:43:14 +0200

    Ajout du squelette de la doc

--- src/components/SwalDocumentation.vue
+++ src/components/SwalDocumentation.vue
@@ -1,23 +1,29 @@
 <template>
-  <div><PDF/></div>
+  <div>
+    <h5>Faire une requête</h5>
+    <p>
+      Pour faire une requête...
+    </p>
+    <h5>Faire une requête spatiale</h5>
+    <p>
+      Pour faire une requête spatiale...
+    </p>
+    <h5>Exemple de requêtes</h5>
+    <p>
+      Voici des exemples de requêtes...
+    </p>
+  </div>
 </template>
 
 <script>
-import PDF from './pdfComponents/PDF.vue'
 
-export default {
-  name: 'SwalManager',
-  components: {
-    PDF
-  },
-}
 </script>
 
 <style scoped>
-
-div {
-    height: 100%;
-    width: 100%;
-}
-
+  h5{
+    text-align: left;
+  }
+  p{
+    text-align: left;
+  }
 </style>
