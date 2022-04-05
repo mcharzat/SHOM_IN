2780756b0a165d76d08abbd644374e5ae35cfb3d
ljeannest <lucie.jeannest@orange.fr>
2022-04-05 15:31:56 +0200

    Add images and first part of the doc

--- src/components/SwalDocumentation.vue
+++ src/components/SwalDocumentation.vue
@@ -1,32 +1,85 @@
 <template>
   <div>
     <img src="../assets/doc/Capture_Nereus_global.png" style="width: 1100px; height: auto; padding-bottom: 30px;">
-    <button id=button_1>1</button>
-    <button id=button_2>2</button>
-    <button id=button_3>3</button>
-    <button id=button_4>4</button>
-    <button id=button_5>5</button>
-    <h5>1- Barre de navigation :</h5>
-    <p>
-      blabla
-    </p>
-    <h5>2- Gestion des couches</h5>
-    <p>
-      blabla
-    </p>
-    <h5>3- Gestion de la carte</h5>
-    <p>
-      blabla
-    </p>
-    <h5>4- Requêtes Sparnatural</h5>
-    <p>
-      blabla
-    </p>
-    <h5>5- Affichage des IN en pdf</h5>
-    <p>
-      blabla
-    </p>
+
+
+    <a href="#part1">
+      <button id=button_1>1</button>
+    </a>
+    <a href="#part2">
+      <button id=button_2>2</button>
+    </a>
+    <a href="#part3">
+      <button id=button_3>3</button>
+    </a>
+    <a href="#part4">
+      <button id=button_4>4</button>
+    </a>
+    <a href="#part5">
+      <button id=button_5>5</button>
+    </a>
+
+
+    <div id=part1>
+      <h5>1- Barre de navigation :</h5>
+      <br>
+      <img src="../assets/doc/NavBar.png" style="height: 75px;">
+      <br><br>
+      <p>
+        On retrouve 3 éléments sur la barre de navigation : <br><br>
+        1. Une option de login :
+      
+      <img src="../assets/doc/NavBar_login.png" style="height: 75px;">
+      </p>
+      <p>
+        Lorsque vous accédez au site, connectez-vous avec vos identifiants du SHOM. Cela vous permettra d'avoir accès aux requêtes et aux pdf des Instructions Nautiques. <br><br>
+
+        2. Un bouton qui permet d'accéder aux sites du SHOM : 
+        <img src="../assets/doc/NavBar_shom.png" style="height: 75px;">
+      </p>
+      <p>
+        En cliquant sur ce bouton, une fenêtre pop-up s'ouvre avec des liens vers les 4 sites du SHOM : <br>
+        &emsp;- Site institutionnel <br>
+        &emsp;- Portail data.shom.fr <br>
+        &emsp;- Espace de diffusion <br>
+        &emsp;- Portail horaire des marées <br><br>
+
+        3. Un bouton qui ramène sur la documentation que vous êtes en train de consulter ! 
+        <img src="../assets/doc/NavBar_help.png" style="height: 75px;">
+      </p>
+    </div>
+
+    <div id=part2>
+      <h5>2- Gestion des couches</h5>
+      <p>
+        Lorem ipsum dolor sit amet. Et assumenda ipsa quo vitae iure et saepe corrupti et voluptatem enim. Et doloribus odio nam illo facilis in galisum natus est magnam ratione vel incidunt perspiciatis et sequi error. Non dolorum pariatur aut blanditiis laudantium non doloremque accusantium aut aperiam placeat hic odit maiores. Ut exercitationem delectus in quia accusamus non ipsa nulla 33 deleniti vero a facilis ratione id voluptatibus dolor. Rem voluptates dolores est dolore omnis sit autem aspernatur qui aliquam illum in voluptatem voluptatum. Qui facilis fuga aut facilis quisquam hic excepturi eius voluptatum reprehenderit est quidem modi eum repellat cumque et dolor rerum. Est fugit explicabo qui dolor optio ut nobis nihil eum quia facere et nihil molestiae ut exercitationem tenetur! Qui quis nobis est maxime suscipit cum rerum quasi ut repellendus consequatur aut officia incidunt est blanditiis voluptatibus non ratione culpa!
+      </p>
+    </div>
+
+    <div id=part3>
+      <h5>3- Gestion de la carte</h5>
+      <p>
+        Lorem ipsum dolor sit amet. Et assumenda ipsa quo vitae iure et saepe corrupti et voluptatem enim. Et doloribus odio nam illo facilis in galisum natus est magnam ratione vel incidunt perspiciatis et sequi error. Non dolorum pariatur aut blanditiis laudantium non doloremque accusantium aut aperiam placeat hic odit maiores. Ut exercitationem delectus in quia accusamus non ipsa nulla 33 deleniti vero a facilis ratione id voluptatibus dolor. Rem voluptates dolores est dolore omnis sit autem aspernatur qui aliquam illum in voluptatem voluptatum. Qui facilis fuga aut facilis quisquam hic excepturi eius voluptatum reprehenderit est quidem modi eum repellat cumque et dolor rerum. Est fugit explicabo qui dolor optio ut nobis nihil eum quia facere et nihil molestiae ut exercitationem tenetur! Qui quis nobis est maxime suscipit cum rerum quasi ut repellendus consequatur aut officia incidunt est blanditiis voluptatibus non ratione culpa!
+      </p>
+    </div>
+
+    <div id=part4>
+      <h5>4- Requêtes Sparnatural</h5>
+      <p>
+        Lorem ipsum dolor sit amet. Et assumenda ipsa quo vitae iure et saepe corrupti et voluptatem enim. Et doloribus odio nam illo facilis in galisum natus est magnam ratione vel incidunt perspiciatis et sequi error. Non dolorum pariatur aut blanditiis laudantium non doloremque accusantium aut aperiam placeat hic odit maiores. Ut exercitationem delectus in quia accusamus non ipsa nulla 33 deleniti vero a facilis ratione id voluptatibus dolor. Rem voluptates dolores est dolore omnis sit autem aspernatur qui aliquam illum in voluptatem voluptatum. Qui facilis fuga aut facilis quisquam hic excepturi eius voluptatum reprehenderit est quidem modi eum repellat cumque et dolor rerum. Est fugit explicabo qui dolor optio ut nobis nihil eum quia facere et nihil molestiae ut exercitationem tenetur! Qui quis nobis est maxime suscipit cum rerum quasi ut repellendus consequatur aut officia incidunt est blanditiis voluptatibus non ratione culpa!
+      </p>
+    </div>
+
+    <div id=part5>
+      <h5>5- Affichage des IN en pdf</h5>
+      <p>
+        Lorem ipsum dolor sit amet. Et assumenda ipsa quo vitae iure et saepe corrupti et voluptatem enim. Et doloribus odio nam illo facilis in galisum natus est magnam ratione vel incidunt perspiciatis et sequi error. Non dolorum pariatur aut blanditiis laudantium non doloremque accusantium aut aperiam placeat hic odit maiores. Ut exercitationem delectus in quia accusamus non ipsa nulla 33 deleniti vero a facilis ratione id voluptatibus dolor. Rem voluptates dolores est dolore omnis sit autem aspernatur qui aliquam illum in voluptatem voluptatum. Qui facilis fuga aut facilis quisquam hic excepturi eius voluptatum reprehenderit est quidem modi eum repellat cumque et dolor rerum. Est fugit explicabo qui dolor optio ut nobis nihil eum quia facere et nihil molestiae ut exercitationem tenetur! Qui quis nobis est maxime suscipit cum rerum quasi ut repellendus consequatur aut officia incidunt est blanditiis voluptatibus non ratione culpa!
+      </p>
+    </div>
+
   </div>
+
+
 </template>
 
 <script>
