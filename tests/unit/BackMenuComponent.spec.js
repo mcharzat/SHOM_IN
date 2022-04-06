8650c2ebb5c9e1d5071e234370018a96c9601449
CMAlla <clairemarie.alla@gmail.com>
2022-04-06 16:40:20 +0200

    feat(backMenu): add a unit test

--- tests/unit/BackMenuComponent.spec.js
+++ tests/unit/BackMenuComponent.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import BackMenu from "@/components/BackMenu.vue";
+import backMenu from "@/components/BackMenu.vue";
 
 describe("BackMenu.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = true;
-    const wrapper = shallowMount(BackMenu, {
+    const wrapper = shallowMount(backMenu, {
       propsData: { widthPdf: msg }
     });
     const BackButton = wrapper.find(".backMenu");
@@ -13,7 +13,10 @@ describe("BackMenu.vue", () => {
     expect(BackButton.attributes("class")).toContain("backMenuOpen");
   });
 
-  it("check click on button", async () => {
+  it("emits a signal on click", async () => {
+    const wrapper = shallowMount(backMenu);
+    await wrapper.find('button').trigger('click');
 
-  });
+    expect(wrapper.emitted().openMenu).toBeTruthy();
+})
 });
