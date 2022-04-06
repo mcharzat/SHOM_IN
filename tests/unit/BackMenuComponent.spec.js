d08b16aa4e60ea4b2c6655bc22fb66c92bbb5037
CMAlla <clairemarie.alla@gmail.com>
2022-04-06 17:10:55 +0200

    bug(backmenutest): resolve tests bugs

--- tests/unit/BackMenuComponent.spec.js
+++ tests/unit/BackMenuComponent.spec.js
@@ -2,20 +2,24 @@ import { shallowMount } from "@vue/test-utils";
 import backMenu from "@/components/BackMenu.vue";
 
 describe("BackMenu.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = true;
-    const wrapper = shallowMount(backMenu, {
-      propsData: { widthPdf: msg }
-    });
-    const BackButton = wrapper.find(".backMenu");
+  const msg = true;
+  const wrapper = shallowMount(backMenu, {
+    propsData: { widthPdf: msg},
+    data() {
+      return {
+        buttonState: true
+      }
+    }
+  });
+  const BackButton = wrapper.find('.backMenu');
 
+  it("renders props.msg when passed", () => {
     expect(wrapper.vm.widthPdf).toEqual(msg);
-    expect(BackButton.attributes("class")).toContain("backMenuOpen");
+    expect(BackButton.attributes("class")).toContain('backMenuOpen');
   });
 
   it("emits a signal on click", async () => {
-    const wrapper = shallowMount(backMenu);
-    await wrapper.find('button').trigger('click');
+    await BackButton.trigger('click');
 
     expect(wrapper.emitted().openMenu).toBeTruthy();
 })
