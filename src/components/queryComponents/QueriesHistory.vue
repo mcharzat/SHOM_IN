a1e451d2e1dd6d49ea164ecbe21066f1095f8700
Maxime_Charzat <23charzatcav@gmail.com>
2022-04-07 16:52:02 +0200

    doc(queriesHistory): Add documentation and commentories

--- src/components/queryComponents/QueriesHistory.vue
+++ src/components/queryComponents/QueriesHistory.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="myQuery">
     <div :class="{name: true, selected: displayed}" @click="updateDisplay">
-      <div v-if="stateNameEdition" @dblclick="editionMode">
+      <div v-if="!stateNameEdition" @dblclick="editionMode">
         {{ nameQuery }}
       </div>
       <div v-else>
@@ -22,6 +22,17 @@
 </template>
 
 <script>
+/**
+ * @module queryHistory
+ * @vue-event {String} renameQuery - New name of the query
+ * @vue-event displayQueryMap - Updated the state of the display on the map of the query
+ * @vue-event displayQueryResult - Refresh the display of entity result of the query
+ * @vue-event removeQuery - Remove the query
+ * @vue-prop {String} [nameQuery=Requête] - Name of the query
+ * @vue-prop {Boolean} [displayed=true] - State of the display on the map of the querey
+ * @vue-data {Boolean} [stateNameEdition=true] - Wether the display of the name is in edition mode
+ * @vue-data {String} newName - New name of the query
+ */
 export default {
     name: "queryHistory",
     emits: ['renameQuery', 'displayQueryMap', 'displayQueryResult', 'removeQuery'],
@@ -37,11 +48,14 @@ export default {
     },
     data() {
         return {
-            stateNameEdition: true,
+            stateNameEdition: false,
             newName: "",
         }
     },
     methods: {
+        /**
+         * Set up the edition mode
+         */
         async editionMode() {
             this.newName = this.nameQuery;
             this.stateNameEdition = !this.stateNameEdition;
@@ -49,19 +63,35 @@ export default {
             await this.$forceUpdate();
             this.$refs['rename'].focus();
         },
+        /**
+         * End the edition mode.
+         * @emits renameQuery
+         */
         submitRename() {
             this.stateNameEdition = !this.stateNameEdition;
             this.$emit('renameQuery', this.newName);
         },
+        /**
+         * Reset the edition mode and cancel the rename.
+         */
         cancelRename() {
             this.stateNameEdition = true;
         },
+        /**
+         * @emits displayQueryMap
+         */
         updateDisplay() {
             this.$emit("displayQueryMap");
         },
+        /**
+         * @emits displayQueryResult
+         */
         displayResult() {
             this.$emit('displayQueryResult');
         },
+        /**
+         * @emits removeQuery
+         */
         removeQuery() {
             this.$emit('removeQuery');
         }
