28acb9bf38f74ecdc6dfde619c44af20976d0381
LePaulM <p.miancien@laposte.net>
2022-04-07 11:17:49 +0200

    satisfaction

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,7 +17,7 @@
     </div>
     <div class=navBarRight>
         <button class="navBarButton menuButton" title='Sites du SHOM' @click.prevent="displayShom">Le SHOM</button>
-        <button class="navBarButton helpButton" title='Aide' @click.prevent="displayHelp">?</button>
+        <button class="helpButton navBarButton" title='Aide' @click.prevent="displayHelp">?</button>
     </div>
 </nav>
 </template>
@@ -121,7 +121,7 @@ export default {
 
   .navBarMiddle{
     position: fixed;
-    right: 175px;
+    right: 200px;
     height: 50px;
     display: flex;
 
@@ -135,18 +135,6 @@ export default {
     margin : auto 10px;
     height: 35px;
     
-  }
-
-  .navBarButton {
-    width : auto;
-    min-width: 40px;
-    margin: auto 3px;
-    padding : 3px;
-    min-height: 100%;
-    position: relative;
-    color: white;
-    background-color: initial;
-    border-style: none;
   }
 
   .logged p{
@@ -166,93 +154,50 @@ export default {
 
   .helpButton{
     font-size:20px;
+    
+    aspect-ratio: 1 / 1;
+    height: 100%;
+
+    border-width: 5px;
     border-radius: 50%;
   }
 
   .menuButton{
     height: 35px;
     width: auto;
-    padding : 3px;
+    padding : 5px;
+
     font-size: 18px;
-    border-radius: 50%;
+    
     vertical-align: middle;
+    
+    border-width: 1px;
+    border-radius: 5%;
   }
   
-
-@media (min-width: 768px) {
   .navBarButton {
-    padding: 10px 20px;
+    width : auto;
+    min-width: 40px;
+    margin: auto 0.5em;
+    padding: 0px 7px;
+    min-height: 35px;
+    position: relative;
+    color: white;
+    background-color: initial;
+
+    border:solid;
+    border-color:white;
+
+    font-size: 1.15em;
+
+    box-shadow: 1px;
   }
-}
 
-.navBarButton:before,
-.navBarButton:after,
-input[type=submit]::before,
-input[type=submit]::after {
-  border-radius: 80px;
-}
+  .navBarButton:hover {
+    background-color: white;
+    color : #076e8c;
+    border-color:rgb(11, 73, 94);
+  }
 
-.navBarButton:before {
-  background-color: white;
-  color : #076e8c;
-  content: "";
-  display: block;
-  height: 100%;
-  left: 0;
-  overflow: hidden;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  z-index: -2;
-}
 
-.navBarButton:after {
-  background-color: #076e8c;
-  bottom: 4px;
-  content: "";
-  display: block;
-  left: 4px;
-  overflow: hidden;
-  position: absolute;
-  right: 4px;
-  top: 4px;
-  transition: all 100ms ease-out;
-  z-index: -1;
-}
-
-.navBarButton:hover:not(:disabled):before {
-  background-color: white;
-  color : #076e8c;
-}
-
-.navBarButton:hover:not(:disabled):after {
-  color : #076e8c;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  top: 0;
-  transition-timing-function: ease-in;
-  opacity: 0;
-}
-
-.navBarButton:active:not(:disabled) {
-  color : #076e8c;
-}
-
-.navBarButton:active:not(:disabled):before {
-  background-image: linear-gradient(#076e8c 100%), linear-gradient(white);
-}
-
-.navBarButton:active:not(:disabled):after {
-  background-image: (#076e8c 100%);
-  bottom: 4px;
-  left: 4px;
-  right: 4px;
-  top: 4px;
-}
-
-.navBarButton:disabled {
-  cursor: default;
-  opacity: .24;
-}
 </style>
