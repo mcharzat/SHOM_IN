89455b0175ca17befdd225c8a3d8dc95bf41ee25
ljeannest <lucie.jeannest@orange.fr>
2022-04-07 12:51:56 +0200

    Replace images in the doc with the new style of the website

--- src/components/SwalDocumentation.vue
+++ src/components/SwalDocumentation.vue
@@ -15,9 +15,6 @@
     <a href="#part4">
       <button id=button_4>4</button>
     </a>
-    <a href="#part5">
-      <button id=button_5>5</button>
-    </a>
 
 
     <div id=part1>
@@ -35,7 +32,7 @@
         Lorsque vous accédez au site, connectez-vous avec vos identifiants du SHOM. Cela vous permettra d'avoir accès aux requêtes et aux pdf des Instructions Nautiques. <br><br>
 
         2. Un bouton qui permet d'accéder aux sites du SHOM : 
-        <img src="../assets/doc/NavBar_shom.png" style="height: 75px;">
+        <img src="../assets/doc/NavBar_shom.png" style="height: 60px;">
       </p>
       <p>
         En cliquant sur ce bouton, une fenêtre pop-up s'ouvre avec des liens vers les 4 sites du SHOM : <br>
@@ -45,26 +42,12 @@
         &emsp;- Portail horaire des marées <br><br>
 
         3. Un bouton qui ramène sur la documentation que vous êtes en train de consulter ! 
-        <img src="../assets/doc/NavBar_help.png" style="height: 75px;">
+        <img src="../assets/doc/NavBar_help.png" style="height: 60px;">
       </p>
     </div>
 
     <div id=part2>
-      <h5>2- Gestion des couches <img src="../assets/doc/Couche.png"> </h5>
-      <p>
-        Le bouton de gestion de couche, lorsqu'il est cliqué, permet de sélectionner les couches à afficher.<br>
-        &emsp;- Hydrography : Carte de profondeur des fonds marins.<br>
-        &emsp;- Soundings : <br>
-        &emsp;- Dangers : Les différents dangers en mer.<br>
-        &emsp;- Restrictions : Différentes restrictions représentées sur la carte.<br>
-        &emsp;- Topography : <br>
-        &emsp;- Aids to navigation : Les bouées et les phares le long de la côte.<br>
-        &emsp;- Metadata : Les métadonnées liées aux zones.<br><br>
-      </p>
-    </div>
-
-    <div id=part3>
-      <h5>3- Gestion de la carte</h5>
+      <h5>2- Gestion de la carte</h5>
       <br>
       <img src="../assets/doc/Gestion_carte.png" style="height: 100px;">
       <br><br>
@@ -83,15 +66,22 @@
       </p>
     </div>
 
-    <div id=part4>
-      <h5>4- Requêtes Sparnatural</h5>
+    <div id=part3>
+      <h5>3- Requêtes et affichage des résultats</h5>
       <p>
-        Lorem ipsum dolor sit amet. Et assumenda ipsa quo vitae iure et saepe corrupti et voluptatem enim. Et doloribus odio nam illo facilis in galisum natus est magnam ratione vel incidunt perspiciatis et sequi error. Non dolorum pariatur aut blanditiis laudantium non doloremque accusantium aut aperiam placeat hic odit maiores. Ut exercitationem delectus in quia accusamus non ipsa nulla 33 deleniti vero a facilis ratione id voluptatibus dolor. Rem voluptates dolores est dolore omnis sit autem aspernatur qui aliquam illum in voluptatem voluptatum. Qui facilis fuga aut facilis quisquam hic excepturi eius voluptatum reprehenderit est quidem modi eum repellat cumque et dolor rerum. Est fugit explicabo qui dolor optio ut nobis nihil eum quia facere et nihil molestiae ut exercitationem tenetur! Qui quis nobis est maxime suscipit cum rerum quasi ut repellendus consequatur aut officia incidunt est blanditiis voluptatibus non ratione culpa!
+        Le bouton de gestion de couche, lorsqu'il est cliqué, permet de sélectionner les couches à afficher.<br>
+        &emsp;- Hydrography : Carte de profondeur des fonds marins.<br>
+        &emsp;- Soundings : <br>
+        &emsp;- Dangers : Les différents dangers en mer.<br>
+        &emsp;- Restrictions : Différentes restrictions représentées sur la carte.<br>
+        &emsp;- Topography : <br>
+        &emsp;- Aids to navigation : Les bouées et les phares le long de la côte.<br>
+        &emsp;- Metadata : Les métadonnées liées aux zones.<br><br>
       </p>
     </div>
 
-    <div id=part5>
-      <h5>5- Affichage des IN en pdf</h5>
+    <div id=part4>
+      <h5>4- Affichage des IN en pdf</h5>
       <p>
         Lorem ipsum dolor sit amet. Et assumenda ipsa quo vitae iure et saepe corrupti et voluptatem enim. Et doloribus odio nam illo facilis in galisum natus est magnam ratione vel incidunt perspiciatis et sequi error. Non dolorum pariatur aut blanditiis laudantium non doloremque accusantium aut aperiam placeat hic odit maiores. Ut exercitationem delectus in quia accusamus non ipsa nulla 33 deleniti vero a facilis ratione id voluptatibus dolor. Rem voluptates dolores est dolore omnis sit autem aspernatur qui aliquam illum in voluptatem voluptatum. Qui facilis fuga aut facilis quisquam hic excepturi eius voluptatum reprehenderit est quidem modi eum repellat cumque et dolor rerum. Est fugit explicabo qui dolor optio ut nobis nihil eum quia facere et nihil molestiae ut exercitationem tenetur! Qui quis nobis est maxime suscipit cum rerum quasi ut repellendus consequatur aut officia incidunt est blanditiis voluptatibus non ratione culpa!
       </p>
@@ -125,25 +115,20 @@
   }
   #button_1{
     position: absolute;
-    left: 562px;
-    top: 85px;
+    left: 480px;
+    top: 82px;
   }
   #button_2{
-    position: absolute;
-    left: 100px;
-    top: 565px;
-  }
-  #button_3{
     position: absolute;
     left: 790px;
     top: 540px;
   }
-  #button_4{
+  #button_3{
     position: absolute;
     left: 857px;
     top: 215px;
   }
-  #button_5{
+  #button_4{
     position: absolute;
     left: 1063px;
     top: 150px;
