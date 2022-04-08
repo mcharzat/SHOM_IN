c076a320c1804d529d1f1e9a638be3d8650b4221
LePaulM <p.miancien@laposte.net>
2022-04-08 13:50:22 +0200

    resolving merging conflicts

--- src/components/SPARQLResearch.vue
+++ src/components/SPARQLResearch.vue
@@ -1,6 +1,6 @@
 <template>
-  <button :class="{researchManager: true, researchManagerOpen: widthResult}" @click="actionResearchPanel">
-    <img src="../assets/research.png" height ="35" width="35"/>
+  <button :class="{researchManager: true, researchManagerOpen: moveButton}" @click="actionResearchPanel">
+    <img src="../assets/research.png" title="Afficher/masquer l'outil de recherche" height ="35" width="35"/>
   </button>
   <div class="research" :hidden="!isDisplayed">
     <QuerySPARQL 
